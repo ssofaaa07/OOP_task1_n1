f0378a4acc0486ac1b178e5bdbe54cc929435b42
ssofaaa07 <s.butovetskaya@gmail.com>
2022-10-13 12:26:12 +0300

    <<>>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,98 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    String str = "uffheofrh fhfdk fhjd{} f";
+
+    public App(String str) {
+        this.str = str;
     }
+
+//    public static void main() {
+//        App str1 = new App("uffheofrh fhfdk fhjd{} f");
+//        System.out.println(str1.index("fhf"));
+//        List<String> list = str1.split(" ");
+//        System.out.println(list);
+//    }
+
+    public int index(String path) {
+        for (int i = 0; i < str.length() - path.length() + 1; i++) {
+            if (str.substring(i, i + path.length()).equals(path)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String join(String s) {
+        return str = str + s;
+    }
+
+    public List<String> split(String path) {
+        List<String> list = new ArrayList<String>();
+        String newStr;
+        int i = 0, k = 0;
+        while (i != str.length() - path.length()) {
+            if (str.substring(i,i+path.length()).equals(path)) {
+                list.add(str.substring(k,i));
+                k = i + path.length();
+                i += path.length();
+            }
+            i++;
+        }
+        if (str.substring(str.length() - path.length(), str.length() - path.length() + 1).equals(path)) {
+            list.add(str.substring(k, str.length() - 1));
+        } else {
+            list.add(str.substring(k));
+        }
+        return list;
+    }
+
+    public String replace(String sample, String replace) {
+        int index = this.index(sample);
+        String newStr;
+        StringBuilder sb = new StringBuilder();
+        int i = 0, k = 0;
+        while (i != str.length() - sample.length()) {
+            if (str.substring(i,i+sample.length()).equals(sample)) {
+                sb.append(str.substring(k,i));
+                sb.append(replace);
+                k = i + sample.length();
+                i += sample.length();
+            }
+            i++;
+        }
+        if (str.substring(str.length() - sample.length(), str.length() - sample.length() + 1).equals(sample)) {
+            sb.append(str.substring(k, str.length() - 1));
+        } else {
+            sb.append(str.substring(k));
+        }
+        newStr = sb.toString();
+        return newStr;
+    }
+
+    public String format(String format) {
+        return this.replace("{}", format);
+    }
+
+    public boolean equals(String path) {
+        if (path.length() != str.length()) {
+            return false;
+        }
+        for (int i = 0; i<str.length(); i++) {
+            if (str.substring(i, i+1).toCharArray()[0] != path.substring(i, i+1).toCharArray()[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,59 @@
 package org.example;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.framework.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+    public void testIndex() {
+        App str = new App("s;nvc s;pifuvni splejn");
+        Assert.assertEquals(8, str.index("pif"));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testJoin() {
+        App str1 = new App("s;nvc s;pifuvni");
+        Assert.assertEquals("s;nvc s;pifuvni s;nvc", str1.join(" s;nvc"));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testSplit() {
+        App str = new App(" s;nvc s;nifuvni splejn ");
+        List<String> list1 = new ArrayList<String>();
+        list1.add("");
+        list1.add("s;nvc");
+        list1.add("s;nifuvni");
+        list1.add("splejn");
+        Assert.assertEquals(list1, str.split(" "));
+        List<String> list2 = new ArrayList<String>();
+        list2.add(" s");
+        list2.add("vc s");
+        list2.add("ifuvni splejn ");
+        Assert.assertEquals(list2, str.split(";n"));
     }
+
+    public void testReplace() {
+        App str = new App("ns;nvc s;nifuvni splejn");
+        Assert.assertEquals("ns;nvc s;1uvni splejn", str.replace("nif", "1"));
+        Assert.assertEquals("ns nvc s nifuvni splejn", str.replace(";", " "));
+        Assert.assertEquals("s;vc s;ifuvi splej", str.replace("n", ""));
+    }
+
+    public void testFormat() {
+        App str = new App("ns;nvc s;nifu{}vni splejn");
+        Assert.assertEquals("ns;nvc s;nifu123vni splejn", str.format("123"));
+    }
+
+    public void testEquals() {
+        App str = new App("ns;nvc");
+        Assert.assertTrue(str.equals("ns;nvc"));
+        Assert.assertFalse(str.equals("ns;nv"));
+    }
+
 }
